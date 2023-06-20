66efe76543cb71921426c5f356353745e20c5a5c
Paweł Czyż <pczyz@protonmail.com>
2023-06-20 16:53:51 +0200

    Export utilities.

---
+++ src/pmhn/__init__.py
@@ -0,0 +1,12 @@
+from pmhn._simulate import (
+    simulate_dataset,
+    simulate_genotype_known_time,
+    simulate_trajectory,
+)
+
+
+__all__ = [
+    "simulate_dataset",
+    "simulate_genotype_known_time",
+    "simulate_trajectory",
+]
