fad478d3257429dc5c53618b9fc30b4afe821bba
Paweł Czyż <pczyz@protonmail.com>
2023-06-21 11:23:05 +0200

    Add documentation.

--- src/pmhn/_backend/_learnmhn.py
+++ src/pmhn/_backend/_learnmhn.py
@@ -28,13 +28,35 @@ def _cast_mutations(mutations: np.ndarray) -> np.ndarray:
 
 
 class _Backend(Protocol):
+    """A backend for learning the MHN model.
+
+    All implementations of this interface must be able to compute
+    the gradient and the loglikelihood of a given set of mutations
+    and theta (log-MHN) matrix.
+    """
+
     def gradient_and_loglikelihood(
         self, mutations: np.ndarray, theta: np.ndarray
     ) -> tuple[np.ndarray, float]:
+        """Compute the gradient and the loglikelihood of a given set of mutations
+
+        Args:
+            mutations, shape (n_patients, n_genes)
+            theta: log-MHN matrix, shape (n_genes, n_genes)
+
+        Returns:
+            gradient of the loglikelihood with respect to theta,
+              shape (n_genes, n_genes)
+            loglikelihood of the given mutations, float
+        """
         ...
 
 
 class JoblibBackend(_Backend):
+    """Calculates the gradient and the loglikelihood
+    by using multiple processes via Joblib, sending
+    them individual patient data."""
+
     def __init__(self, n_jobs: int = DEFAULT_N_JOBS) -> None:
         self._pool = joblib.Parallel(n_jobs=n_jobs)
 
@@ -56,6 +78,9 @@ class JoblibBackend(_Backend):
 
 
 class MHNCythonBackend(_Backend):
+    """A simple wrapper around the Cython implementation
+    of the gradient and loglikelihood."""
+
     @staticmethod
     def gradient_and_loglikelihood(
         mutations: np.ndarray, theta: np.ndarray
