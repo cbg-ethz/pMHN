3e9f9ff0bba99d4aaaa6644e2fb56f8fbae06aae
Paweł Czyż <pczyz@protonmail.com>
2023-12-06 23:25:54 +0100

    Add the loglikelihood function

--- src/pmhn/_trees/_backend_jax/_loglikelihood.py
+++ src/pmhn/_trees/_backend_jax/_loglikelihood.py
@@ -1,6 +1,7 @@
 from jaxtyping import Array, Float
 
 from pmhn._trees._backend_jax._rates import _construct_log_magic_matrix
+from pmhn._trees._backend_jax._solver import logprob_forward_substitution
 from pmhn._trees._backend_jax._wrapper import WrappedTree
 
 
@@ -13,13 +14,13 @@ def loglikelihood(
     # Construct the "magic matrix":
     # log(V) on the diagonal,
     # log(-V) = log(Q) off the diagonal.
-    _construct_log_magic_matrix(
+    log_magic = _construct_log_magic_matrix(
         tree=tree,
         theta=theta,
         omega=omega,
         log_tau=log_tau,
     )
-
-    # TODO(Pawel): UNTESTED
-    # TODO(Pawel): NOT-IMPLEMENTED
-    pass
+    # Calculate the log-probability of all trees
+    log_probs = logprob_forward_substitution(log_magic)
+    # Return the log-probability of the last tree
+    return log_probs[-1]
