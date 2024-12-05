2565af343667c1264079f631b92c5ed558a05380
Paweł Czyż <pczyz@protonmail.com>
2024-12-05 20:37:50 +0100

    Remove the dependencies from the package source

--- src/pmhn/__init__.py
+++ src/pmhn/__init__.py
@@ -3,32 +3,15 @@ from pmhn._simulate import (
     simulate_genotype_known_time,
     simulate_trajectory,
 )
-from pmhn._backend import (
-    MHNCythonBackend,
-    MHNJoblibBackend,
-    MHNBackend,
-    control_no_mutation_warning,
-)
-from pmhn._ppl import (
-    MHNLoglikelihood,
-    PersonalisedMHNLoglikelihood,
-    prior_regularized_horseshoe,
-    prior_normal,
-    prior_only_baseline_rates,
-    prior_offdiagonal_laplace,
-    prior_spike_and_slab_marginalized,
-    prior_horseshoe,
-)
 from pmhn._theta import construct_matrix, decompose_matrix, sample_spike_and_slab
 from pmhn._visualise import (
-    plot_genotypes,
     plot_genotype_samples,
-    plot_theta,
-    plot_offdiagonal_sparsity,
+    plot_genotypes,
     plot_offdiagonal_histograms,
+    plot_offdiagonal_sparsity,
+    plot_theta,
     plot_theta_samples,
 )
-
 
 __all__ = [
     "simulate_dataset",
